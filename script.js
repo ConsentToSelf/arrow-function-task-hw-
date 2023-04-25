7dc1342bb03ee4402b44675886369c32e064a9e3
Султан <carameleclipsimus@gmail.com>
2023-04-25 10:56:44 +0600

    feat: first task of arrow-function is done

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ return params.reduce(function (prev, next) { return prev + next; });
 sum(1, 2, 3, 4); // 10
 sum(); // 0
 */
-const sum = () => {
-  const params = Array.prototype.slice.call(arguments);
-  return params;
+const sum = (...arguments) => {
+  let value = [...arguments];
+  if (!value.length) return 0;
+  return value.reduce((prev, next) => {
+    return prev + next;
+  });
 };
-
-console.log(sum(1, 2, 3, 4));
+console.log(sum());
