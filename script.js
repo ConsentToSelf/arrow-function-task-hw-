ef02afe25aa45b366549cb196d9bcc5e4501f00a
Султан <carameleclipsimus@gmail.com>
2023-04-25 10:59:59 +0600

    feat: second task of this is done

--- script.js
+++ script.js
@@ -12,11 +12,36 @@ return params.reduce(function (prev, next) { return prev + next; });
 sum(1, 2, 3, 4); // 10
 sum(); // 0
 */
-const sum = (...arguments) => {
-  let value = [...arguments];
-  if (!value.length) return 0;
-  return value.reduce((prev, next) => {
-    return prev + next;
-  });
-};
-console.log(sum());
+// const sum = (...arguments) => {
+//   let value = [...arguments];
+//   if (!value.length) return 0;
+//   return value.reduce((prev, next) => {
+//     return prev + next;
+//   });
+// };
+// console.log(sum());
+
+// second
+
+/*
+Переделать функцию с использованием функции-стрелки
+function convertToObject(num) {
+  const obj = {
+
+      value: num,
+
+      isOdd: Boolean(num % 2)
+
+  }
+
+  return obj;
+
+}
+
+*/
+const convertToObject = (num) => ({
+  value: num,
+  isOdd: Boolean(num % 2),
+});
+
+console.log(convertToObject(5));
